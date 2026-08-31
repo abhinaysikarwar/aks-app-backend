6cf5d4de6a82672b67fd79a45f54ea7e5b89103e
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-08-31 20:28:45 +0530

    update

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "http://localhost:8081",
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/api/health")
 def health():
